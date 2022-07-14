a6f7744a2097b10598c8f2487716ac6f3d630c39
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2022-07-14 17:09:10 +0200

    add fuzz helper scripts

---
+++ scripts/fuzz_run.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+set -ev
+
+# Start from a build directory, usually clang_cpp17_release
+# usage: fuzz_run.sh <testname>
+FUZZ_TARGET=$1
+SCRIPT_DIR=`dirname "$0"`
+CORPUS_SMALL=${SCRIPT_DIR}/../data/fuzz/${FUZZ_TARGET}
+CORPUS_BIG=CORPUS_${FUZZ_TARGET}
+NUM_JOBS=$(nproc)
+
+mkdir -p ${CORPUS_BIG}
+ninja
+chrt -i 0 ./test/fuzz_${FUZZ_TARGET} -jobs=${NUM_JOBS} -workers=${NUM_JOBS} ${CORPUS_BIG} ${CORPUS_SMALL}
