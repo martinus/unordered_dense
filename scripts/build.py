85f49768285df772af80e3f7ab94003c6dd939c0
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2022-07-17 19:46:42 +0200

    use ccache

--- scripts/build.py
+++ scripts/build.py
@@ -6,31 +6,31 @@ import subprocess
 
 
 cmd_and_dir = [
-    ['env', 'CXX="clang++"', 'meson', 'setup', '--buildtype', 'release', '-Dcpp_std=c++17', 'builddir/clang_cpp17_release'],
-    ['env', 'CXX="clang++"', 'meson', 'setup', '--buildtype', 'debug',   '-Dcpp_std=c++17', 'builddir/clang_cpp17_debug'],
-    ['env', 'CXX="g++"',     'meson', 'setup', '--buildtype', 'release', '-Dcpp_std=c++17', 'builddir/gcc_cpp17_release'],
-    ['env', 'CXX="g++"',     'meson', 'setup', '--buildtype', 'debug',   '-Dcpp_std=c++17', 'builddir/gcc_cpp17_debug'],
+    ['env', 'CXX=ccache clang++', 'meson', 'setup', '--buildtype', 'release', '-Dcpp_std=c++17', 'builddir/clang_cpp17_release'],
+    ['env', 'CXX=ccache clang++', 'meson', 'setup', '--buildtype', 'debug',   '-Dcpp_std=c++17', 'builddir/clang_cpp17_debug'],
+    ['env', 'CXX=ccache g++',     'meson', 'setup', '--buildtype', 'release', '-Dcpp_std=c++17', 'builddir/gcc_cpp17_release'],
+    ['env', 'CXX=ccache g++',     'meson', 'setup', '--buildtype', 'debug',   '-Dcpp_std=c++17', 'builddir/gcc_cpp17_debug'],
 
     # 32bit. Install lib32-clang
-    ['env', 'CXX="g++"',     'meson', 'setup', '--buildtype', 'debug', '-Dcpp_std=c++17', '-Dcpp_args="-m32"', '-Dcpp_link_args="-m32"', '-Dc_args="-m32"', '-Dc_link_args="-m32"', 'builddir/gcc_cpp17_debug_32'],
-    ['env', 'CXX="clang++"', 'meson', 'setup', '--buildtype', 'debug', '-Dcpp_std=c++17', '-Dcpp_args="-m32"', '-Dcpp_link_args="-m32"', '-Dc_args="-m32"', '-Dc_link_args="-m32"', 'builddir/clang_cpp17_debug_32'],
+    ['env', 'CXX=ccache g++',     'meson', 'setup', '--buildtype', 'debug', '-Dcpp_std=c++17', '-Dcpp_args="-m32"', '-Dcpp_link_args="-m32"', '-Dc_args="-m32"', '-Dc_link_args="-m32"', 'builddir/gcc_cpp17_debug_32'],
+    ['env', 'CXX=ccache clang++', 'meson', 'setup', '--buildtype', 'debug', '-Dcpp_std=c++17', '-Dcpp_args="-m32"', '-Dcpp_link_args="-m32"', '-Dc_args="-m32"', '-Dc_link_args="-m32"', 'builddir/clang_cpp17_debug_32'],
 
     # c++20
-    ['env', 'CXX="clang++"', 'meson', 'setup', '--buildtype', 'debug', '-Dcpp_std=c++20', 'builddir/clang_cpp20_debug'],
-    ['env', 'CXX="g++"',     'meson', 'setup', '--buildtype', 'debug', '-Dcpp_std=c++20', 'builddir/gcc_cpp20_debug'],
+    ['env', 'CXX=ccache clang++', 'meson', 'setup', '--buildtype', 'debug', '-Dcpp_std=c++20', 'builddir/clang_cpp20_debug'],
+    ['env', 'CXX=ccache g++',     'meson', 'setup', '--buildtype', 'debug', '-Dcpp_std=c++20', 'builddir/gcc_cpp20_debug'],
 
     # coverage; use "ninja clean && ninja test && ninja coverage"
-    ['env', 'CXX="clang++"', 'meson', 'setup', '-Db_coverage=true', 'builddir/coverage'],
+    ['env', 'CXX=ccache clang++', 'meson', 'setup', '-Db_coverage=true', 'builddir/coverage'],
 
     # sanitizers
     # It is not possible to combine more than one of the -fsanitize=address, -fsanitize=thread, and -fsanitize=memory checkers in the same program.
     # see https://clang.llvm.org/docs/UsersManual.html#controlling-code-generation
     #
     # can't use ccache, it doesn't work with the ignorelist.txt
-    ['env', 'CXX="clang++"', 'meson', 'setup', '-Db_sanitize=address',   'builddir/clang_sanitize_address'],
-    ['env', 'CXX="clang++"', 'meson', 'setup', '-Db_sanitize=thread',    'builddir/clang_sanitize_thread'],
-    # ['env', 'CXX="clang++" meson setup -Db_sanitize=memory',    'builddir/clang_sanitize_memory], # doesn't work due to STL, and ignore doesn't work either :-(
-    ['env', 'CXX="clang++"', 'meson', 'setup', '-Db_sanitize=undefined', 'builddir/clang_sanitize_undefined'],
+    ['env', 'CXX=ccache clang++', 'meson', 'setup', '-Db_sanitize=address',   'builddir/clang_sanitize_address'],
+    ['env', 'CXX=ccache clang++', 'meson', 'setup', '-Db_sanitize=thread',    'builddir/clang_sanitize_thread'],
+    # ['env', 'CXX=ccache clang++ meson setup -Db_sanitize=memory',    'builddir/clang_sanitize_memory], # doesn't work due to STL, and ignore doesn't work either :-(
+    ['env', 'CXX=ccache clang++', 'meson', 'setup', '-Db_sanitize=undefined', 'builddir/clang_sanitize_undefined'],
 ]
 
 root_path = Path(__file__).parent.parent
