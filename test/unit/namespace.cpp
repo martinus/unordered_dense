3b463e7154f2ad4163b5988a023587e08ebca034
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2022-09-01 13:42:48 +0200

    add versioning namespace with a test

---
+++ test/unit/namespace.cpp
@@ -0,0 +1,11 @@
+#include <ankerl/unordered_dense.h>
+
+#include <doctest.h>
+
+static_assert(std::is_same_v<ankerl::unordered_dense::v1_3_0::map<int, int>, ankerl::unordered_dense::map<int, int>>);
+static_assert(std::is_same_v<ankerl::unordered_dense::v1_3_0::hash<int>, ankerl::unordered_dense::hash<int>>);
+
+TEST_CASE("version_namespace") {
+    auto map = ankerl::unordered_dense::v1_3_0::map<int, int>{};
+    REQUIRE(map.empty());
+}
