875f6d269b4e5c8c7c320604743237e14a552089
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2022-12-12 15:40:36 +0100

    unordered_dense::set: iterator is now const_iterator
    Note that pointer and reference are still as before. This is the same
    as for std::unordered_set.

--- include/ankerl/unordered_dense.h
+++ include/ankerl/unordered_dense.h
@@ -396,12 +396,12 @@ template <class Key,
           class KeyEqual,
           class AllocatorOrContainer,
           class Bucket>
-class table : public std::conditional_t<std::is_void_v<T>, base_table_type_set, base_table_type_map<T>> {
+class table : public std::conditional_t<is_map_v<T>, base_table_type_map<T>, base_table_type_set> {
 public:
     using value_container_type = std::conditional_t<
         is_detected_v<detect_iterator, AllocatorOrContainer>,
         AllocatorOrContainer,
-        typename std::vector<typename std::conditional_t<std::is_void_v<T>, Key, std::pair<Key, T>>, AllocatorOrContainer>>;
+        typename std::vector<typename std::conditional_t<is_map_v<T>, std::pair<Key, T>, Key>, AllocatorOrContainer>>;
 
 private:
     using bucket_alloc =
@@ -423,8 +423,8 @@ public:
     using const_reference = typename value_container_type::const_reference;
     using pointer = typename value_container_type::pointer;
     using const_pointer = typename value_container_type::const_pointer;
-    using iterator = typename value_container_type::iterator;
     using const_iterator = typename value_container_type::const_iterator;
+    using iterator = std::conditional_t<is_map_v<T>, typename value_container_type::iterator, const_iterator>;
     using bucket_type = Bucket;
 
 private:
@@ -491,10 +491,10 @@ private:
     }
 
     [[nodiscard]] static constexpr auto get_key(value_type const& vt) -> key_type const& {
-        if constexpr (std::is_void_v<T>) {
-            return vt;
-        } else {
+        if constexpr (is_map_v<T>) {
             return vt.first;
+        } else {
+            return vt;
         }
     }
 
@@ -1202,6 +1202,7 @@ public:
         return begin() + static_cast<difference_type>(value_idx_to_remove);
     }
 
+    template <typename Q = T, std::enable_if_t<is_map_v<Q>, bool> = true>
     auto erase(const_iterator it) -> iterator {
         return erase(begin() + (it - cbegin()));
     }
@@ -1432,14 +1433,14 @@ public:
         }
         for (auto const& b_entry : b) {
             auto it = a.find(get_key(b_entry));
-            if constexpr (std::is_void_v<T>) {
-                // set: only check that the key is here
-                if (a.end() == it) {
+            if constexpr (is_map_v<T>) {
+                // map: check that key is here, then also check that value is the same
+                if (a.end() == it || !(b_entry.second == it->second)) {
                     return false;
                 }
             } else {
-                // map: check that key is here, then also check that value is the same
-                if (a.end() == it || !(b_entry.second == it->second)) {
+                // set: only check that the key is here
+                if (a.end() == it) {
                     return false;
                 }
             }
