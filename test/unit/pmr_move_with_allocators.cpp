135b11e0daa29214a5e483257bd212613e3d804a
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2023-07-15 12:56:32 +0200

    extended pmr test, fixed another allocator issue in segmented_vector

--- test/unit/pmr_move_with_allocators.cpp
+++ test/unit/pmr_move_with_allocators.cpp
@@ -8,14 +8,11 @@
 // windows' vector has different allocation behavior, macos has linker errors
 #    if __linux__
 
-using int_str_map = ankerl::unordered_dense::pmr::map<int, std::string>;
-using int_str_map_segmented = ankerl::unordered_dense::pmr::segmented_map<int, std::string>;
-
 namespace {
 
 // creates a map and moves it out
 template <typename Map>
-auto return_hello_world(ANKERL_UNORDERED_DENSE_PMR::memory_resource* resource) {
+auto return_hello_world(ANKERL_UNORDERED_DENSE_PMR::memory_resource* resource) -> Map {
     Map map_default_resource(resource);
     map_default_resource[0] = "Hello";
     map_default_resource[1] = "World";
@@ -24,28 +21,33 @@ auto return_hello_world(ANKERL_UNORDERED_DENSE_PMR::memory_resource* resource) {
 
 template <typename Map>
 auto doTest() {
-    Map map_default_resource(ANKERL_UNORDERED_DENSE_PMR::new_delete_resource());
-
     ANKERL_UNORDERED_DENSE_PMR::synchronized_pool_resource pool;
 
-    // segfaults if m_buckets is reused
     {
-        map_default_resource = return_hello_world<Map>(&pool);
-        REQUIRE(map_default_resource.contains(0));
+        Map m(ANKERL_UNORDERED_DENSE_PMR::new_delete_resource());
+        m = return_hello_world<Map>(&pool);
+        REQUIRE(m.contains(0));
     }
 
     {
-        Map map_default_resource_construct(return_hello_world<Map>(&pool), ANKERL_UNORDERED_DENSE_PMR::new_delete_resource());
-        REQUIRE(map_default_resource.contains(0));
+        Map m(ANKERL_UNORDERED_DENSE_PMR::new_delete_resource());
+        m[0] = "foo";
+        m = return_hello_world<Map>(&pool);
+        REQUIRE(m[0] == "Hello");
+    }
+
+    {
+        Map m(return_hello_world<Map>(&pool), ANKERL_UNORDERED_DENSE_PMR::new_delete_resource());
+        REQUIRE(m.contains(0));
     }
 }
 
 TEST_CASE("move_with_allocators") {
-    doTest<int_str_map>();
+    doTest<ankerl::unordered_dense::pmr::map<int, std::string>>();
 }
 
 TEST_CASE("move_with_allocators_segmented") {
-    doTest<int_str_map_segmented>();
+    doTest<ankerl::unordered_dense::pmr::segmented_map<int, std::string>>();
 }
 
 } // namespace
