58eb6bc2395e9925c00b5c85f90b0ad85568f5fa
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2023-12-22 18:20:52 +0100

    Add extract(key) and extract(it) API.
    Similar to erase(), but the return value is the erased element, moved out.
    This has practically the same performance behavior as erase(), except
    for 2 additional moves. (once into a temporary variable), and then out.

--- test/unit/extract.cpp
+++ test/unit/extract.cpp
@@ -3,6 +3,8 @@
 #include <app/counter.h>
 #include <app/doctest.h>
 
+#include <fmt/format.h>
+
 TEST_CASE_MAP("extract", counter::obj, counter::obj) {
     auto counts = counter();
     INFO(counts);
@@ -23,4 +25,38 @@ TEST_CASE_MAP("extract", counter::obj, counter::obj) {
         REQUIRE(container[i].first.get() == i);
         REQUIRE(container[i].second.get() == i);
     }
+}
+
+TEST_CASE_MAP("extract_element", counter::obj, counter::obj) {
+    auto counts = counter();
+    INFO(counts);
+
+    counts("init");
+    auto map = map_t();
+    for (size_t i = 0; i < 100; ++i) {
+        map.try_emplace(counter::obj{i, counts}, i, counts);
+    }
+
+    // extract(key)
+    for (size_t i = 0; i < 20; ++i) {
+        auto query = counter::obj{i, counts};
+        counts("before remove 1");
+        auto opt = map.extract(query);
+        counts("after remove 1");
+        REQUIRE(opt);
+        REQUIRE(opt->first.get() == i);
+        REQUIRE(opt->second.get() == i);
+    }
+    REQUIRE(map.size() == 80);
+
+    // extract iterator
+    for (size_t i = 20; i < 100; ++i) {
+        auto query = counter::obj{i, counts};
+        auto it = map.find(query);
+        REQUIRE(it != map.end());
+        auto opt = map.extract(it);
+        REQUIRE(opt.first.get() == i);
+        REQUIRE(opt.second.get() == i);
+    }
+    REQUIRE(map.empty());
 }
